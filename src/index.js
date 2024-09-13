f6073a720767c3c164463a9f18a351c1d2f6994e
Lucasyankauskas <yankauskas_lucas1@hotmail.com>
2024-09-13 18:08:40 -0300

    7 test

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 import express from 'express';
 import {dirname, join} from 'path';
 import {fileURLToPath} from 'url';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 import indexRoutes from './routes/index.js';
-
+import { config } from 'process';
 
 
 const app = express();
 
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.set('views', join(__dirname, 'views'));
+app.set('views', join(__dirname, 'views' ,));
 app.set('view engine', 'ejs');
 app.use(indexRoutes);
+
 
 app.use(express.static(join(__dirname, 'public')));
 
 app.listen(process.env.PORT || 3000);
+
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const router = Router();
 
+const formapi = process.env.FORM_KEY;
 
 router.get('/', (req, res) => res.render('index', {
-    title: `Haltbar`,
+    title: `Haltbar`, formkey : `${formapi}` ,
 }));
 
 router.get('/about', (req, res) => res.render('about', {
